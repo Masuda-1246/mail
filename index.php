277b72074bbe710a02148e3a7ba68972abd3560b
Yosuke <yousuke@ageet.com>
2022-04-23 20:10:53 +0900

    2nd commit

--- index.php
+++ index.php
@@ -5,15 +5,6 @@
 </head>
 <body>
 <form action="send.php" method="post">
-件名：<br />
-<input type="text" name="subject" size="30" value=""/><br />
-送信者名：<br />
-<input type="text" name="name" size="30" value="" /><br />
-メールアドレス：<br />
-<input type="text" name="mail" size="30" value="" /><br />
-本文：<br />
-<textarea name="message" cols="30" rows="5"></textarea><br />
-<br />
 <input type="submit" value="送信する" />
 </form>
 </body>
